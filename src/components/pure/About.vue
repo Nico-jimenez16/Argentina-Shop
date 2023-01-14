ec537e2987d38db1eb0c7c918c8e08dadd855ee0
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2023-01-14 00:33:05 -0300

    about page

---
+++ src/components/pure/About.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="bg-black p-12">
+        <div class="contenedor">
+            <table>
+                <tr>
+                    <td><div class="circulo n1"> 
+                        <div class="absolute w-full h-full flex justify-center items-center">
+                            <h1 class="text-3xl text-white">Proyectos</h1>
+                        </div> 
+                    </div></td>
+                    <td><div class="circulo n2">
+                        <div class="absolute w-full h-full flex justify-center items-center">
+                            <h1 class="text-md text-black">E-Commerce - ReactJs</h1>
+                        </div>
+                    </div></td>
+                </tr>
+                <tr>
+                    <td><div class="circulo n3">
+                        <div class="absolute w-full h-full flex justify-center items-center">
+                            <h1 class="text-md text-black">Backend</h1>
+                        </div>
+                    </div></td>
+                    <td><div class="circulo n4">
+                        <div class="absolute w-full h-full flex justify-center items-center">
+                            <h1 class="text-md text-black">E- Commerce - VueJs</h1>
+                        </div>
+                    </div></td>
+                </tr>
+                <tr>
+                    <td><div class="circulo n5">
+                        <div class="absolute w-full h-full flex justify-center items-center">
+                            <h1 class="text-md text-black">Argentina Shop - Vue Js</h1>
+                        </div>
+                    </div></td>
+                    <!-- <td><div class="circulo n6">
+                        <div class="absolute w-full h-full flex justify-center items-center">
+                            <h1 class="text-md text-black">Hola soy nicolas</h1>
+                        </div>
+                    </div></td> -->
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.contenedor{
+    width: 700px;
+    height: 700px;
+    border: 1px solid white;
+    margin: auto;
+    display: grid;
+    grid-column: 2;
+    border-radius: 50%;
+    animation: rotacion 50s infinite linear;
+}
+.circulo{
+    width: 200px;
+    height: 200px;
+    background: white;
+    border-radius: 50%;
+    margin: 0px 10px 0px 10px;
+    position: relative;
+    background: rgb(238, 207, 207);
+    animation: rotar 50s infinite linear;
+}
+
+@keyframes rotacion {
+    from{
+        transform: rotate(0deg);
+    }
+    to{
+        transform: rotate(360deg);
+    }
+}
+@keyframes rotar {
+    from{
+        transform: rotate(360deg);
+    }
+    to{
+        transform: rotate(0deg);
+    }
+}
+
+td{
+    width: 100px;
+    height: 100px;
+}
+.n1{
+    float: center;
+    margin-top: 10px;
+    background: black;
+    border: 1px solid white;
+}
+.n2{
+    float: left;
+    margin-top: 10px;
+    margin-left: -30%;
+}
+.n3{
+    float: left;
+}
+.n4{
+    float: right;
+}
+.n5{
+    float: right;
+    margin-top: -20px;
+    margin-right: -30%;
+}
+.n6{
+    float: left;
+    margin-top: -20px;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,26 @@
 <script setup>
+import About from '../components/pure/About.vue';
 </script>
 
 
 <template>
     <div class="mt-16">
-        <div>
-            <h1>About</h1>
-            <h1>agregar otros proyecto y una descripcion mia ,  ademas de las tecnologia empleadas para el proyecto.</h1>
+        <div class="w-full h-auto flex flex-col justify-center items-center description">
+            <h1 class="w-full flex pl-4 text-3xl mt-8 underline">Angel Nicolas Jimenez</h1>
+            <h1 class="m-4">Hola a todos , mi nombre es Nicolas soy estudiante de Ingenieria en Sistemas de Informacion en la Universidad Tecnológica Nacional de Cordoba, Argentina. Actualmente me encuentro cursando Tercer año.
+                Experiencia profecional:
+                Frontend developer en LeadboxHq. LeadboxHq es una empresa canadiense de la industria automotriz. El puesto que me desempeñe es desarrolador frontend en el area de soporte, construyendo páginas y resolviendo los pedidos de los clientes.
+                Actualmente trabajo en Creativos Digitales como profesor de programacion para chicos.
+                Tengo muchas ganas de seguir aprendiendo y sumando experiencia como desarrollador y cómo profesional y llegar a ser un FullStack Developer.</h1>
         </div>
+        <About />
     </div>
 </template>
+
+
+<style>
+.description{
+    background: rgb(242, 222, 222);
+}
+
+</style>
